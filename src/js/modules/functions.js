ed78a044e8e3fd58603f1723900a5c3e38925cee
caiman <shimyshimya@mail.ru>
2023-02-07 21:44:48 +0500

    adaptive

--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -12,3 +12,32 @@ export function isWebp() {
         document.documentElement.classList.add(className)
     });
 }
+
+export function burger() {
+    let b = document.querySelector('.burger');
+    let nav = document.querySelector('.header__nav');
+    let links = document.querySelectorAll('.header__item');
+    let body = document.querySelector('body');
+    
+    b.addEventListener('click', e => {
+        
+        if (b.classList.contains('active')) {
+            b.classList.remove('active')
+            nav.classList.remove('active')
+            body.classList.remove('lock')
+        } else {
+            b.classList.add('active')
+            nav.classList.add('active')
+            body.classList.add('lock')
+        }
+        
+    })
+    
+    links.forEach(link => {
+        link.addEventListener('click', e => {
+            b.classList.remove('active')
+            nav.classList.remove('active')
+            body.classList.remove('lock')
+        })
+    })
+}
